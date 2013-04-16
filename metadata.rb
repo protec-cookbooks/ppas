24efc5f6589d412461ba50540b3f57f74f751c3b
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2013-04-16 15:50:01 +0100

    update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
+maintainer       "Protec Innovations"
+maintainer_email "dev@protecinnovations.co.uk"
+license          "BSD"
 description      "Installs/Configures ppas"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
