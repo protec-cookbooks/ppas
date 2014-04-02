00983d6e600461a6c615c398a77dafae1d027137
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2014-04-02 15:38:38 +0100

    revert

--- recipes/mez-php.rb
+++ recipes/mez-php.rb
@@ -1,10 +1,9 @@
-unless node['platform_version'].to_f < 13.10
-	apt_repository "php-mez" do
-	  uri "http://ppa.launchpad.net/mez/php/ubuntu"
-	  distribution node['lsb']['codename']
-	  components ["main"]
-	  keyserver "keyserver.ubuntu.com"
-	  key "B7AC0613"
-	  action :add
-	end
+
+apt_repository "php-mez" do
+	uri "http://ppa.launchpad.net/mez/php/ubuntu"
+	distribution node['lsb']['codename']
+	components ["main"]
+	keyserver "keyserver.ubuntu.com"
+	key "B7AC0613"
+	action :add
 end
