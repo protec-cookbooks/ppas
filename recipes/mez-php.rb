c65e4440b6d076ad8dfc8802f01e62e707990a5e
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2014-04-02 13:06:24 +0100

    Update so it doesn't have issues on Saucy

--- recipes/mez-php.rb
+++ recipes/mez-php.rb
@@ -1,8 +1,10 @@
-apt_repository "php-mez" do
-  uri "http://ppa.launchpad.net/mez/php/ubuntu"
-  distribution node['lsb']['codename']
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-  key "B7AC0613"
-  action :add
+unless node['platform_version'].to_f < 13.10
+	apt_repository "php-mez" do
+	  uri "http://ppa.launchpad.net/mez/php/ubuntu"
+	  distribution node['lsb']['codename']
+	  components ["main"]
+	  keyserver "keyserver.ubuntu.com"
+	  key "B7AC0613"
+	  action :add
+	end
 end
