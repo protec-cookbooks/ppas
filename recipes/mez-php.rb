ca0556077cae518877bf627ec399db8922b8efe8
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2014-04-29 12:36:10 +0100

    Only install for certain versions

--- recipes/mez-php.rb
+++ recipes/mez-php.rb
@@ -1,9 +1,13 @@
-
-apt_repository "php-mez" do
-	uri "http://ppa.launchpad.net/mez/php/ubuntu"
-	distribution node['lsb']['codename']
-	components ["main"]
-	keyserver "keyserver.ubuntu.com"
-	key "B7AC0613"
-	action :add
+case node['platform']
+when 'ubuntu'
+    unless node['platform_version'].to_f >= 13.10
+		apt_repository "php-mez" do
+			uri "http://ppa.launchpad.net/mez/php/ubuntu"
+			distribution node['lsb']['codename']
+			components ["main"]
+			keyserver "keyserver.ubuntu.com"
+			key "B7AC0613"
+			action :add
+		end
+    end
 end
