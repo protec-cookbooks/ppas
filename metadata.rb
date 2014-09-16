9a3f6484e79af2a0a2e4abe8bf572f512e94e664
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2014-09-16 14:18:56 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "dev@protecinnovations.co.uk"
 license          "BSD"
 description      "Installs/Configures ppas"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 name "ppas" 
 
