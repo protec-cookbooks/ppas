b1a5ba1bc18a601807c835af466a5b62fa4a4856
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2015-01-08 14:14:14 +0000

    Update to 5.6

--- recipes/ondrej.rb
+++ recipes/ondrej.rb
@@ -1,5 +1,5 @@
 apt_repository "php-ondrej" do
-  uri "http://ppa.launchpad.net/ondrej/php5/ubuntu"
+  uri "http://ppa.launchpad.net/ondrej/php5-5.6/ubuntu"
   distribution node['lsb']['codename']
   components ["main"]
   keyserver "keyserver.ubuntu.com"
